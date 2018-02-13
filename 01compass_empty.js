83314deb0ce19c0707937e85d921188d17f173be
Gaga <Gaga@JiayingdeMacBook-Pro.local>
2018-02-13 14:16:10 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,6 +8,9 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
+
+        if(compassHeading > 315 || compassHeading < 45) {
+
          text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
